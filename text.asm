ff22c1a13156b0f4a1e24971d722a4328dd6957b
CrissisCore <79920481+kakarottto@users.noreply.github.com>
2025-04-13 21:34:17 +0000

    added jmp instruction

--- text.asm
+++ text.asm
@@ -1,15 +1,23 @@
+;abba nigga
+;sudalulla 
+;hello ; world
 
-abba nigga
-sudalulla 
-hello ; world
-
-sup nigga ;idk sfasf as ffasf 	
-idk;man
+;sup nigga ;idk sfasf as ffasf 	
+;idk;man
 ;mov ax, 1000
 ;mov eax,4294967296
 mov rax,18446744073709551615
 mov al, 0xfb
 and al, 0xf0
 
+jmp nigga
 
+gaga:mov bx, 6688
+deba : mov ax, 9669
+hello:	
+
+nigga: mov cx, 1000
+
+jmp gaga
+;deba:
 
